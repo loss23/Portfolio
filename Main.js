ee37ecc39b822a8b299a4c7f1d1fbdfcc20ffd22
loss23 <mebob248888@gmail.com>
2022-10-11 14:19:40 -0500

    f

--- Main.js
+++ Main.js
@@ -7,3 +7,11 @@
 function RobloxAccount(){
     window.location.assign("https://www.roblox.com/users/2911604233/profile");
 }
+
+function Pinguin(){
+    window.location.href = "pinguin.html";
+}
+
+function Main(){
+    window.location.href = "index.html";
+}
