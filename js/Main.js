70c3a375ade3b6e172583fa42e9f986642ed2987
loss23 <61843498+loss23@users.noreply.github.com>
2023-03-11 19:27:29 -0800

    a

---
+++ js/Main.js
@@ -0,0 +1,9 @@
+function home() {
+    window.location.assign("index.html")
+}
+function contact() {
+    window.location.assign("/Pages/Contact.html")
+}
+function info() {
+    window.location.assign("/Pages/Info.html")
+}
