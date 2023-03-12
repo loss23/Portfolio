5d7c78bf32cc81f718208b34d80cabc15b3ec6f2
loss23 <61843498+loss23@users.noreply.github.com>
2023-03-11 20:00:22 -0800

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -1,9 +1,9 @@
 function home() {
-    window.location.assign("index.html")
+    window.location.assign("../index.html")
 }
 function contact() {
-    window.location.assign("/Pages/Contact.html")
+    window.location.assign("/pages/Contact.html")
 }
 function info() {
-    window.location.assign("/Pages/Info.html")
+    window.location.assign("/pages/Info.html")
 }
